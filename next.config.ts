43226a463c599cc6049ab7bff7b552107f3e18c2
Varad Srivastava <varadsrivastavaofficial@gmail.com>
2026-01-29 12:24:39 +0530

    Set basePath and assetPrefix for Portfolio

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
   images: {
     unoptimized: true,
   },
