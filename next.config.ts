f69362a049809043f07dc8b73762d0b82942e8f0
Varad Srivastava <varadsrivastavaofficial@gmail.com>
2026-01-29 12:04:21 +0530

    Remove basePath and assetPrefix from config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Portfolio',
-  assetPrefix: '/Portfolio/',
   images: {
     unoptimized: true,
   },
